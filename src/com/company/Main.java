f372b6430fe60362392eeba8bd222533581a734b
glebklimakhin <klimakhiny@mail.ru>
2020-10-28 19:50:04 +0300

    hw1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	Robot robot = new Robot("Fedor", 15, 10);
 	Cat cat = new Cat("Melon", 5,6);
-	Human human = new Human("Charles", 20, 32);
+	Human human = new Human("Steve", 20, 32);
 	Treadmill treadmill = new Treadmill(14);
 	Wall wall = new Wall(14);
 	cat.run(treadmill);
